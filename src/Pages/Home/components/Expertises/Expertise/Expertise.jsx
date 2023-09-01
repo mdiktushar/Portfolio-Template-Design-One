777484695ae8184735209d450e57cf91e41a5a41
mdiktushar <md.tushar1804@gmail.com>
2023-09-01 19:14:52 +0600

    expertise section complete

--- src/Pages/Home/components/Expertises/Expertise/Expertise.jsx
+++ src/Pages/Home/components/Expertises/Expertise/Expertise.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Expertise = ({expertise}) => {
     const {imageUrl, name, description} = expertise
     return (
-        <div className='bg-[#F5FCFF] p-6 rounded-lg'>
+        <div className='p-6 col-span-4 md:col-span-2 lg:col-span-1 rounded-lg bg-[#F5FCFF] '>
             <img className='mb-4 p-2 bg-white shadow-lg rounded-md' src={imageUrl} alt={name} width={'50px'} />
             <h3 className='mb-4 text-xl font-bold'>{name}</h3>
             <p>{description}</p>
--- src/Pages/Home/components/Expertises/Expertises.jsx
+++ src/Pages/Home/components/Expertises/Expertises.jsx
@@ -35,10 +35,10 @@ const Expertises = () => {
     );
   }
   return (
-    <div className="m-24">
-      <p>My Skills</p>
-      <h2>My Expertise</h2>
-      <div className="flex gap-5">
+    <div className="m-16 md:m-18 lg:m-24">
+      <p className="font-bold text-xs">My Skills</p>
+      <h2 className="mt-2 mb-12 font-bold text-3xl">My Expertise</h2>
+      <div className="grid grid-cols-4 gap-5">
         {expertises.map((expertise, index) => (
           <Expertise key={index} expertise={expertise} />
         ))}
