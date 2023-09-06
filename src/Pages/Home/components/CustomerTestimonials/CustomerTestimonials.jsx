f0534563ac0e77f5125481f06dd57ab0fb82b51c
mdiktushar <md.tushar1804@gmail.com>
2023-09-06 17:21:02 +0600

    feedBack cart

--- src/Pages/Home/components/CustomerTestimonials/CustomerTestimonials.jsx
+++ src/Pages/Home/components/CustomerTestimonials/CustomerTestimonials.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import FeedBack from './Feedback/FeedBack';
 
 const CustomerTestimonials = ({feedbacks}) => {
     return (
         <div className='bg-[#F5FCFF]'>
             <p>Clients Feedback</p>
             <h3>Customer testimonials</h3>
+
+            <div>
+                <FeedBack />
+            </div>
         </div>
     );
 };
